60132d19b1efb89cae27d44363b7025f2e0ba7ef
Thorodinson1 <maniwertyu123@gmail.com>
2023-10-17 16:57:31 +0530

    ini

--- RebahinProvider/src/main/kotlin/com/hexated/Cgvindo.kt
+++
@@ -1,9 +0,0 @@
-package com.hexated
-
-import com.lagradost.cloudstream3.TvType
-
-class Cgvindo : RebahinProvider() {
-    override var mainUrl = "http://cgvindo.ink"
-    override var name = "Cgvindo"
-
-}
